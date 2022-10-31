3e6e9aeb197f7de4d820b9f1adab71b590d20cd9
Nils Niggemann <nils.niggemann@fu-berlin.de>
2022-10-31 12:17:48 +0100

    moved most methods to FRGLatticeEvaluation

--- src/FRGLatticePlotting.jl
+++ src/FRGLatticePlotting.jl
@@ -1,22 +1,8 @@
 module FRGLatticePlotting
     using SpinFRGLattices,Parameters,StaticArrays,LaTeXStrings,Plots
 
-    using SLEEFPirates:cos_fast
-
-    include("Fourier.jl")
-    export AbstractLattice, LatticeInfo,FourierTransform, Fourier2D, Chikplot, getFlow, plotFlow, getMaxFlow, plotMaxFlow,plotMaxFlow!,plotMaxFlow_fast, pointPath, fetchKPath, plotKpath,plotKpath!, pscatter!,pplot!,getkMax,Fourier3D
-
-    
     include("LatticePlot.jl")
     
     export pairsPlot, plotSystem, plotCouplings!,plotCorrelations!,plotBond!,plotBonds!,plotDistBonds!,plotDistBonds,getVertexR
-    
-    include("helpers.jl")
-    export hhlplane,normalTohhlplane,xyplane,zzerocut,sphereplane,hhllabels,xylabels,zzerolabels,normalTohhllabels,strd,strd3,getPairs,DimerResponse,DimerResponse_Keles,allOccurIn,findNames,NameFilter,OnlyIndex,getNumberFromName,stringLatex,getFourier
-    
 
-    include("Tests/FourierTest.jl")
-    export test_fourier_onsite,test_fourier_pairs
-
-    
 end # module
