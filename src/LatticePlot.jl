fcaf1b34755b14e5235317a0f092dd15f9d5cb00
Nils Niggemann <nils.niggemann@fu-berlin.de>
2023-08-05 13:35:47 +0200

    data inspector

--- src/LatticePlot.jl
+++ src/LatticePlot.jl
@@ -9,10 +9,15 @@ linesRvec!(Rs::AbstractVector{<:Rvec},Basis,args...;kwargs...) = lines!(current_
 scatterRvec(Rs::AbstractVector{<:Rvec},Basis,args...;kwargs...) = scatter(getPoint.(Rs,Ref(Basis)),args...;kwargs...)
 lineRvecs(Rs::AbstractVector{<:Rvec},Basis,args...;kwargs...) = lines(getPoint.(Rs,Ref(Basis)),args...;kwargs...)
 
-function pairsPlot!(ax,PairList,Basis,args...;colors = [:blue,:red,:black,:cyan,:yellow,:green,:pink,:orange,:lime,:brown,:grey],colorBasis = false,kwargs...)
+function pairsPlot!(ax,PairList,Basis,args...;colors = [:black,:red,:blue,:cyan,:yellow,:green,:pink,:orange,:lime,:brown,:grey,:darkred],colorBasis = false,kwargs...)
     uniquepairs = unique(PairList)
     
-    inds = [Basis.SiteType[R.b] for R in uniquepairs]
+    inds = 
+    if colorBasis
+        [R.b for R in uniquepairs]
+    else
+        [Basis.SiteType[R.b] for R in uniquepairs]
+    end
     col = colors[inds]
 
     scatterRvec!(ax,uniquepairs,Basis,color = col,args...;kwargs...)
@@ -99,19 +104,50 @@ function plotDistBonds(SiteList::AbstractVector{<:Rvec},Basis,args...;kwargs...)
     fig
 end
 
+getstring(R::Rvec_3D) = string("(",R.n1,", ",R.n2,", ",R.n3,", ",R.b,")")
+getstring(R::Rvec_2D) = string("(",R.n1,", ",R.n2,", ",R.b,")")
+
+function getInspector(RvecList::AbstractVector{<:Rvec},Basis)
+    function inspector(self, i, p)
+        R = RvecList[i]
+        r = round.(getPoint(R,Basis),digits=3)
+        string(getstring(R), " =\n",r)
+    end
+    return inspector
+end
+
+
+function getPairNumberInspector(PairList::AbstractVector{<:Rvec},Basis)
+    function inspector(self, i, p)
+        R = PairList[i]
+        # r = round.(getPoint(R,Basis),digits=3)
+        string(getstring(R)," -> ",i)
+    end
+    return inspector
+end
+
+function getCorrelationInspector(J,R2)
+    function inspector(self, i, p)
+        string("J(i,",getstring(R2)," = ",J)
+    end
+    return inspector
+end
+
 """Plot all sites and inequivalent pairs"""
 function plotSystem(System,Basis,args...;
     plotAll = true,
     refSite = nothing,
-    markersize = 15,
+    markersize = 20,
     inequivColor = :green,
     inequivalpha = 0.7,
     plotBonds=true,
     plotCouplings=true,
+    colorBasis = false,
     CouplingColors = nothing,
     Bonds = [(minDist = Basis.NNdist-1e-3,maxDist = Basis.NNdist+1e-3,colorRGB = [0,0,0])],
     bondlw = 7,
     allpairs = unique!(SpinFRGLattices.sortedPairList(System.NLen,Basis)[1]),
+    inspect = true,
     kwargs...)
     (;PairList,OnsitePairs )= System
     
@@ -126,47 +162,57 @@ function plotSystem(System,Basis,args...;
 
     plotAll || (allpairs = plotpairs)
     fig,ax = getStandardFigure(first(plotpairs))
+    
+    if inspect 
+        DataInspector(fig)
+    end
 
     if plotBonds
         if bondlw isa Real
             bondlw = [bondlw for b in Bonds]
         end
         for (lw,b) in zip(bondlw,Bonds)
-            plotDistBonds!(ax,allpairs,Basis,minDist = b.minDist, maxDist = b.maxDist;lw,color = Makie.Colors.RGB((b.colorRGB./255)...))
+            plotDistBonds!(ax,allpairs,Basis,minDist = b.minDist, maxDist = b.maxDist;lw,color = Makie.Colors.RGB((b.colorRGB./255)...),inspectable = false)
         end
     end
 
     
-    plotAll && pairsPlot!(ax,plotpairs,Basis,color = inequivColor,alpha = inequivalpha,markersize = 2*markersize)
+    linewidthscaling = J -> 3*abs(J)
+    inverseScaling = val -> 1/3 * val
 
     if refSite !== nothing
-        plotCouplings && plotCorrelations!(ax,System,Basis,System.couplings;allpairs,refSite = Basis.refSites[refSite],colors = CouplingColors)
+        plotCouplings && plotCorrelations!(ax,System,Basis,System.couplings;allpairs,refSite = Basis.refSites[refSite],colors = CouplingColors,linewidthscaling)
     end
     
-    pairsPlot!(ax,allpairs,Basis,markersize = markersize;kwargs...)
-    refSite !== nothing && scatterRvec!(ax,[Basis.refSites[refSite]],Basis,color = :darkred,marker = '×',markersize = 3.5*markersize)
+    plotAll && pairsPlot!(ax,plotpairs,Basis,color = inequivColor,alpha = inequivalpha,markersize = 3.5*markersize,inspector_label = getPairNumberInspector(plotpairs,Basis);kwargs...)
 
+    pairsPlot!(ax,allpairs,Basis,markersize = markersize;inspector_label = getInspector(allpairs,Basis),inspectable = true,kwargs...)
+
+    refSite !== nothing && scatterRvec!(ax,[Basis.refSites[refSite]],Basis,color = :darkred,marker = '×',markersize = 5.0*markersize, inspector_label = (self,i,p) -> "refSite" )
+    
     # refSite !== nothing && scatterRvec!(ax,[Basis.refSites[refSite]], Basis)
 
     return fig
 end
 
-function plotCorrelations!(ax,System,Basis,Correlations::AbstractVector,args...; allpairs = unique!(SpinFRGLattices.sortedPairList(System.NLen,Basis)[1]),minCorr = 1e-14,refSite = nothing,colors = nothing,kwargs...)
+function plotCorrelations!(ax,System,Basis,Correlations::AbstractVector,args...; allpairs = unique!(SpinFRGLattices.sortedPairList(System.NLen,Basis)[1]),minCorr = 1e-14,refSite = nothing,colors = nothing,
+    linewidthscaling = J -> 3*abs(J),
+    kwargs...)
 
     (;PairTypes,PairList) = System
 
     inds = findall(x-> abs(x)>minCorr,Correlations)
     Correlations = Correlations[inds]
     PairList = PairList[inds]
-    label(J) = round(J,digits=3)
-    lw(J) = 0.1+3*abs(J)
+    label(J) = string(round(J,digits=3))
 
     for i in eachindex(Correlations,PairList)
         J = Correlations[i]
         color = J > 0 ? :red : :blue
-        linesRvec!(ax,[refSite,PairList[i]],Basis;color,linewidth = lw(J),args...,kwargs...)
+        R = PairList[i]
+        linesRvec!(ax,[refSite,R],Basis;color,label = label(J),linewidth = linewidthscaling(J),inspector_label = getCorrelationInspector(J,R),args...,kwargs...)
     end
-
+    axislegend(ax)
     return ax
     
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,8 @@
 using FRGLatticePlotting, SpinFRGLattices
-
-S = Pyrochlore.getPyrochlore(3, [5.,-3,0,1]./2)
-plotSystem(S,Pyrochlore.Basis,refSite = 1)
+S = Pyrochlore.getPyrochlore(3, [5.,-3,0,1])
+fig = plotSystem(S,Pyrochlore.Basis,refSite = 1,colorBasis=false,inspect = true)
+# x = DataInspector(fig)
+# fig
+##
+S = SquareKagome.getMirrorSquareKagome(3, 5.,-3)
+fig = plotSystem(S,SquareKagome.Basis,refSite = 1,inspect = true)
